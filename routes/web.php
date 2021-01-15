42a511cd2f9e5cb0d899fbb5817408e54c422c02
vagrant <vagrant@homestead>
2021-01-15 10:23:40 +0000

    分类话题列表

--- routes/web.php
+++ routes/web.php
@@ -24,5 +24,6 @@ Route::post('email/resend', 'Auth\VerificationController@resend')->name('verific
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
-
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+Route::resource('categories', 'categoriesController', ['only' => ['show']]);
