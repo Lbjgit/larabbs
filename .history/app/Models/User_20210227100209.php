949943c9260c1c0ae91503aad6f51b9a5823a4ad
vagrant <vagrant@homestead>
2021-02-27 03:10:55 +0000

    消息通知列表

--- .history/app/Models/User_20210227100209.php
+++ .history/app/Models/User_20210227100209.php
@@ -28,6 +28,21 @@ class User extends Authenticatable implements MustVerifyEmailContract
         'email_verified_at' => 'datetime',
     ];
 
+    public function notify($instance)
+    {
+        // 如果要通知的人是当前用户，就不必通知了！
+        if ($this->id == Auth::id()) {
+            return;
+        }
+
+        // 只有数据库类型通知才需提醒，直接发送 Email 或者其他的都 Pass
+        if (method_exists($instance, 'toDatabase')) {
+            $this->increment('notification_count');
+        }
+
+        $this->laravelNotify($instance);
+    }
+
     public function topics()
     {
         return $this->hasMany(Topic::class);
@@ -41,5 +56,12 @@ class User extends Authenticatable implements MustVerifyEmailContract
     public function replies()
     {
         return $this->hasMany(Reply::class);
+    }
+
+    public function markAsRead()
+    {
+        $this->notification_count = 0;
+        $this->save();
+        $this->unreadNotifications->markAsRead();
     }
 }
